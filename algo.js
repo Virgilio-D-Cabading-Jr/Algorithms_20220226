10240c7b3ee015e97c5680967690e1bee7719b56
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-02-27 21:27:33 -0800

    add comment

--- algo.js
+++ algo.js
@@ -12,6 +12,8 @@
 
     let front = 0;
     let back = strX.length - 1;
+
+    // iterate through the string
     while (front < back) {
         if ( strX[front] != strX[back] ) {
             return false;
